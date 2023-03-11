78f9fa15432f3f85bf3df81d1987e30128c92f93
Jean <jeanspynu@yandex.ru>
2023-03-11 10:38:42 +0200

    Create ResultModelOfT.cs

---
+++ ResultModelOfT.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace GR.Core.Helpers
+{
+    public class ResultModel<T> : IResultModel<T>
+    {
+        /// <inheritdoc />
+        /// <summary>
+        /// Bool indicating that the request resulted with success.
+        /// If False than the <see cref="T:GR.Core.Helpers.ErrorModel" /> will 
+        /// contain a Error message that produced this error.
+        /// </summary>
+        [JsonProperty("is_success")]
+        public virtual bool IsSuccess { get; set; }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// This property will contain error keys if any.
+        /// </summary>
+        [JsonProperty("error_keys")]
+        public virtual ICollection<IErrorModel> Errors { get; set; } = new List<IErrorModel>();
+
+        /// <inheritdoc />
+        /// <summary>
+        /// The result of the response, if there is no errors.
+        /// </summary>
+        [JsonProperty("result")]
+        public virtual T Result { get; set; }
+
+        [JsonIgnore]
+        public Guid? KeyEntity { get; set; }
+
+        /// <summary>
+        /// To base
+        /// </summary>
+        /// <returns></returns>
+        public ResultModel ToBase() => new ResultModel
+        {
+            IsSuccess = IsSuccess,
+            Result = Result,
+            Errors = Errors,
+            KeyEntity = KeyEntity
+        };
+
+        /// <summary>
+        /// Adapt
+        /// </summary>
+        /// <typeparam name="TModelOutput"></typeparam>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        public virtual ResultModel<TModelOutput> Map<TModelOutput>(TModelOutput result = default) => new ResultModel<TModelOutput>
+        {
+            IsSuccess = IsSuccess,
+            Result = result,
+            Errors = Errors
+        };
+
+        private static ResultModel<T> _instance;
+        public static ResultModel<T> Instance => _instance ?? (_instance = new ResultModel<T>());
+
+        /// <summary>
+        /// Add error from results
+        /// </summary>
+        /// <param name="results"></param>
+        /// <returns></returns>
+        public virtual ResultModel<T> JoinResults(IEnumerable<ResultModel> results)
+        {
+            var collection = results.ToList();
+            var response = new ResultModel<T>
+            {
+                IsSuccess = collection.All(x => x.IsSuccess)
+            };
+            foreach (var error in collection.SelectMany(result => result.Errors))
+            {
+                response.Errors.Add(error);
+            }
+
+            return response;
+        }
+    }
+}
